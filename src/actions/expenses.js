33fa362b7950fdc25585fa9a91254d9ef668d589
Nicolas Baranowski <admin@Nicolass-MacBook-Pro.local>
2018-05-22 15:37:41 +0200

    Setup test database env

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,4 +1,5 @@
 import uuid from 'uuid';
+
 import database from '../firebase/firebase';
 
 //ADD_EXPENSE
@@ -8,33 +9,32 @@ export const addExpense = (expense) => ({
 })
 
 export const startAddExpense = (expenseData = {}) => {
-    return (dispatch) => {
-      const {
-        description = '',
-        note = '',
-        amount = 0,
-        createdAt = 0
-      } = expenseData;
-      const expense = { description, note, amount, createdAt };
-  
-      return database.ref('expenses').push(expense).then((ref) => {
-        dispatch(addExpense({
-          id: ref.key,
-          ...expense
-        }));
-      });
-    };
-  };
+  return (dispatch) => {
+    const {
+      description = '',
+      note = '',
+      amount = 0,
+      createdAt = 0
+    } = expenseData;
+    const expense = { description, note, amount, createdAt };
 
+    return database.ref('expenses').push(expense).then((ref) => {
+      dispatch(addExpense({
+        id: ref.key,
+        ...expense
+      }));
+    });
+  };
+};
 //REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({ //this is removeExpense Generator
-type: 'REMOVE_EXPENSE',
-id
+  type: 'REMOVE_EXPENSE',
+  id
 });
 
 //EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
-type: 'EDIT_EXPENSE',
-id,
-updates
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,14 @@ const path = require('path');
 const webpack = require('webpack')
 const extractTextPlugin = require('extract-text-webpack-plugin');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development' //production on heroku
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
     require('dotenv').config({ path: '.env.test' });
-    process.env.FIREBASE_API_KEY
-} else if (process.env.NODE_ENV === 'development') {
+  } else if (process.env.NODE_ENV === 'development') {
     require('dotenv').config({ path: '.env.development' });
-}
-
+  }
+  
 // process.env.NODE_ENV
 
 module.exports = (env) => {
@@ -56,13 +55,13 @@ module.exports = (env) => {
             CSSExtract,
             new webpack.DefinePlugin({
                 'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-                'FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-                'FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-                'FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-                'FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-                'FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
-            })
-        ],
+                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+              })
+            ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
